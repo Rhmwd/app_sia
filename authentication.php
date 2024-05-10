58b3fce5161deb3c2af133349a3865ffe74045dd
Rhmwd <rahmazahra202@gmail.com>
2024-05-10 21:29:24 +0700

    update file

--- authentication.php
+++ authentication.php
@@ -1,20 +1,20 @@
 <?php
  if($_SERVER["REQUEST_METHOD"]=="POST"){
- require_once('koneksi.php');
+require_once('koneksi.php');
  session_start();
  $username = $_POST['username'];
- $password = $_POST['password'];
- $query = "SELECT * FROM tb_pengguna WHERE username='$username'";
+  $password = $_POST['password'];
+$query = "SELECT * FROM tb_pengguna WHERE username='$username'";
  $result = $koneksi->query($query);
  $row = $result->fetch_assoc();
  if(mysqli_num_rows($result)>0){
- if(password_verify($password,$row['password'])){
+if(password_verify($password,$row['password'])){
  $_SESSION['username']=$row['username'];
  $_SESSION['nama_lengkap']=$row['nama_lengkap'];
  $_SESSION['jabatan']=$row['jabatan'];
  $_SESSION['hak_akses']=$row['hak_akses'];
  header('location:dashboard.php');
- }else{
+}else{
  $_SESSION['pesan']="username atau password tidak valid!!!";
  header('location:index.php');
  }
@@ -25,3 +25,4 @@
  }else{
  header('location:index.php');
 }
+
--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db = "app_sia";
-$koneksi = new mysqli($host, $user, $pass, $db);
-?>
+ $user = "root";
+ $pass = "";
+ $db = "app_sia";
+ $koneksi = new mysqli($host, $user, $pass, $db);
+ ?>
