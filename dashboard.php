dca017de9bbea0a44b3639a5b69db1a36d846cd3
Rhmwd <rahmazahra202@gmail.com>
2024-05-30 18:26:20 +0700

    edit file

--- dashboard.php
+++ dashboard.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['username'])) {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
  <body>
@@ -42,7 +42,7 @@ if (!isset($_SESSION['username'])) {
  </li>
  <li>
  <a class="dropdown-item" href="logout.php">
- <i class="bi bi-door-closed-fill"></i>
+ <i class="bi bi-box-arrow-left"></i>
  Logout
  </a>
  </li>
